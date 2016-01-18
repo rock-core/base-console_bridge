ec27fb614847b65281a3f9913a36225b2b9654d4
Janosch Machowinski <janosch.machowinski@dfki.de>
2016-01-18 10:15:50 +0100

    Update console bridge to version 0.3

--- src/console.h
+++ src/console.h
@@ -3,12 +3,76 @@
 
 #include <base/Logging.hpp>
 
-#define logDebug(FORMAT, ARGS...) do { LOG_DEBUG(FORMAT, ## ARGS); } while(0)
-#define logInfo(FORMAT, ARGS...) do { LOG_INFO(FORMAT, ## ARGS); } while(0)
-#define logWarn(FORMAT, ARGS...) do { LOG_WARN(FORMAT, ## ARGS); } while(0)
-#define logError(FORMAT, ARGS...) do { LOG_ERROR(FORMAT, ## ARGS); } while(0)
-#define logFatal(FORMAT, ARGS...) do { LOG_FATAL(FORMAT, ## ARGS); } while(0)
-#define logInform(FORMAT, ARGS...) do { LOG_INFO(FORMAT, ## ARGS); } while(0)
+
+#ifdef __GNUC__
+#define CONSOLE_BRIDGE_DEPRECATED __attribute__ ((deprecated))
+#elif defined(_MSC_VER)
+#define CONSOLE_BRIDGE_DEPRECATED __declspec(deprecated)
+#else
+#pragma message("WARNING: You need to implement DEPRECATED for this compiler")
+#define CONSOLE_BRIDGE_DEPRECATED
+#endif
+
+static inline void CONSOLE_BRIDGE_DEPRECATED console_bridge_deprecated() {}
+
+/** \file console.h
+    \defgroup logging Logging Macros
+    \{
+
+    \def logError(fmt, ...)
+    \brief Log a formatted error string.
+    \remarks This macro takes the same arguments as <a href="http://www.cplusplus.com/reference/clibrary/cstdio/printf">printf</a>.
+
+    \def logWarn(fmt, ...)
+    \brief Log a formatted warning string.
+    \remarks This macro takes the same arguments as <a href="http://www.cplusplus.com/reference/clibrary/cstdio/printf">printf</a>.
+
+    \def logInform(fmt, ...)
+    \brief Log a formatted information string.
+    \remarks This macro takes the same arguments as <a href="http://www.cplusplus.com/reference/clibrary/cstdio/printf">printf</a>.
+
+    \def logDebug(fmt, ...)
+    \brief Log a formatted debugging string.
+    \remarks This macro takes the same arguments as <a href="http://www.cplusplus.com/reference/clibrary/cstdio/printf">printf</a>.
+
+    \}
+*/
+#define CONSOLE_BRIDGE_logError(fmt, ...) do { \
+    LOG_ERROR(fmt, ##__VA_ARGS__); } while(0)
+
+#define CONSOLE_BRIDGE_logWarn(fmt, ...) do { \
+    LOG_WARN(fmt, ##__VA_ARGS__); } while(0)
+
+#define CONSOLE_BRIDGE_logInform(fmt, ...) do { \
+    LOG_INFO(fmt, ##__VA_ARGS__); } while(0)
+
+#define CONSOLE_BRIDGE_logDebug(fmt, ...) do { \
+    LOG_DEBUG(fmt, ##__VA_ARGS__); } while(0)
+
+
+#define logDebug(FORMAT, ARGS...) do { \
+    console_bridge_deprecated(); \
+    LOG_DEBUG(FORMAT, ## ARGS); } while(0)
+
+#define logInfo(FORMAT, ARGS...) do { \
+    console_bridge_deprecated(); \
+    LOG_INFO(FORMAT, ## ARGS); } while(0)
+    
+#define logWarn(FORMAT, ARGS...) do { \
+    console_bridge_deprecated(); \
+    LOG_WARN(FORMAT, ## ARGS); } while(0)
+
+#define logError(FORMAT, ARGS...) do { \
+    console_bridge_deprecated(); \
+    LOG_ERROR(FORMAT, ## ARGS); } while(0)
+
+#define logFatal(FORMAT, ARGS...) do { \
+    console_bridge_deprecated(); \
+    LOG_FATAL(FORMAT, ## ARGS); } while(0)
+
+#define logInform(FORMAT, ARGS...) do { \
+    console_bridge_deprecated(); \
+    LOG_INFO(FORMAT, ## ARGS); } while(0)
 
 
 /** \brief Message namespace. This contains classes needed to
