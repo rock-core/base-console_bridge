4815304b84a7e0bd774751c30caf413b0b1f5ca0
Sylvain <sylvain.joyeux@tidewise.io>
2021-09-29 15:37:46 -0300

    fix: export a proper CMake target in console_bridgeConfig
    This is now needed by e.g. urdfdom

--- src/console_bridgeConfig.cmake
+++ src/console_bridgeConfig.cmake
@@ -1,3 +1,9 @@
 find_package(Rock)
 include(FindPkgConfig)
 rock_find_pkgconfig(console_bridge REQUIRED console_bridge)
+
+add_library(console_bridge::console_bridge INTERFACE IMPORTED GLOBAL)
+target_include_directories(console_bridge::console_bridge INTERFACE
+    ${console_bridge_INCLUDE_DIRS})
+target_link_libraries(console_bridge::console_bridge INTERFACE
+    ${console_bridge_LIBRARIES})
