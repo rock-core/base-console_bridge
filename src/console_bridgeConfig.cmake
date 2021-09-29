6370267e2e73be4a82e82e72bcac6b27d60808dd
Sylvain <sylvain.joyeux@tidewise.io>
2021-09-29 16:05:15 -0300

    fix: compatibility with Ubuntu 18.04

--- src/console_bridgeConfig.cmake
+++ src/console_bridgeConfig.cmake
@@ -3,7 +3,9 @@ include(FindPkgConfig)
 rock_find_pkgconfig(console_bridge REQUIRED console_bridge)
 
 add_library(console_bridge::console_bridge INTERFACE IMPORTED GLOBAL)
-target_include_directories(console_bridge::console_bridge INTERFACE
-    ${console_bridge_INCLUDE_DIRS})
+set_target_properties(console_bridge::console_bridge PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${console_bridge_INCLUDE_DIRS}"
+)
+
 target_link_libraries(console_bridge::console_bridge INTERFACE
     ${console_bridge_LIBRARIES})
